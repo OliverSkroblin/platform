f50a9fd0d558e868540a99746abcb7c2e39648cf
Stephan Franck <stephan@vierpunkt.de>
2022-05-14 13:21:02 +0000

    wip: load filter on demand on open filter dropdown

--- src/Storefront/Resources/app/storefront/src/plugin/listing/filter-property-select.plugin.js
+++ src/Storefront/Resources/app/storefront/src/plugin/listing/filter-property-select.plugin.js
@@ -1,3 +1,4 @@
+import HttpClient from 'src/service/http-client.service';
 import FilterMultiSelectPlugin from 'src/plugin/listing/filter-multi-select.plugin'
 import Iterator from 'src/helper/iterator.helper';
 import DomAccess from 'src/helper/dom-access.helper';
@@ -7,7 +8,58 @@ export default class FilterPropertySelectPlugin extends FilterMultiSelectPlugin 
 
     static options = deepmerge(FilterMultiSelectPlugin.options, {
         propertyName: '',
+        onDemand: false,
+        propertyId: '',
+        listingOptionIds: [],
+        propertyDropdownContainerClass: '.filter-multi-select-dropdown',
+        propertyLoadingSelector: '.filter-property-loading'
     });
+
+    init() {
+        super.init();
+
+        this._client = new HttpClient();
+
+        // get refernces to the dom elements
+        this.dropdownContainer = this.el.querySelectorAll(this.options.propertyDropdownContainerClass)[0];
+    }
+
+    _registerEvents() {
+        if ( this.options.onDemand ) {
+            const collapse = this.el;
+
+            /** @deprecated tag:v6.5.0 - Bootstrap v5 uses native HTML elements and events to subscribe to Collapse plugin events */
+            if (Feature.isActive('v6.5.0.0')) {
+                collapse.addEventListener('show.bs.dropdown', this._onDropdownShow.bind(this));
+            } else {
+                const $collapse = $(collapse);
+                $collapse.on('show.bs.dropdown', this._onDropdownShow.bind(this));
+            }
+        } else {
+            super._registerEvents();
+        }
+    }
+
+    _onDropdownShow() {
+        if ( this.el.querySelectorAll(this.options.propertyLoadingSelector).length > 0 ) {
+            this._client.get('/widgets/properties/' + this.options.propertyId, this._setPropertyOptions.bind(this));
+        }
+    }
+
+    _setPropertyOptions(data) {
+        this.dropdownContainer.innerHTML = data;
+
+        const checkboxes = DomAccess.querySelectorAll(this.el, this.options.checkboxSelector);
+
+        Iterator.iterate(checkboxes, (checkbox) => {
+            if ( !this.options.listingOptionIds.includes(checkbox.id) ) {
+                const listItem = checkbox.closest(this.options.listItemSelector);
+                listItem.parentNode.removeChild(listItem);
+            }
+        });
+
+        super._registerEvents();
+    }
 
     /**
      * @return {Array}
